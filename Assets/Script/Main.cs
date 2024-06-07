42e75a9568de924977a17a8431a56d72350bd8da
dowell <27514076@qq.com>
2024-06-07 11:36:49 +0800

    资源加载 Resources.Load

--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -11,16 +11,8 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    
-    
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
